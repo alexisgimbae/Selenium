ef1f3419d3eff427863f754287d49bd70b6097dc
Alexis Giménez <alexisgimbaez@gmail.com>
2024-01-21 12:00:56 -0300

    New Windows and New Tab - functions for Browser windows

--- TallerSelenium/src/main/java/BrowserMethods.java
+++ TallerSelenium/src/main/java/BrowserMethods.java
@@ -1,14 +1,16 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 
 public class BrowserMethods {
     public static String URL_TEST = "http://jotajotavm.com/";
 
     public static void main(String[] args) throws Exception {
         WebDriver driver = common.ClassConfiguration.getChromeDriver();
+        driver.manage().window().maximize();
         driver.get(URL_TEST);
+        Thread.sleep(3000);
         driver.navigate().to("https://www.google.com/");
 
-        driver.manage().window().maximize();
         Thread.sleep(3000);
         driver.navigate().back();
         Thread.sleep(3000);
@@ -17,6 +19,16 @@ public class BrowserMethods {
         driver.navigate().refresh();
 
         String currentWindowID = driver.getWindowHandle();
+        Thread.sleep(3000);
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        Thread.sleep(3000);
+        driver.switchTo().window(currentWindowID);
+        Thread.sleep(3000);
+        driver.switchTo().newWindow(WindowType.TAB);
+        String newWindows = driver.getWindowHandle();
+        driver.get("https://github.com/casivaagustin-zz/drupal-services/commits?author=casivaagustin-zz");
+        Thread.sleep(3000);
+        driver.switchTo().window(newWindows).close();
         System.out.println("current window id is\t" + currentWindowID);
         driver.quit();
     }
